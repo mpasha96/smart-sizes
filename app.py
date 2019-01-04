3e82c8335aa1da89d758fe96e4b6086f71edb04f
mpasha96 <mukarram.pasha96@gmail.com>
2019-01-04 18:11:03 +0500

    Fixed something else inputs

--- app.py
+++ app.py
@@ -2,16 +2,18 @@ from flask import Flask, render_template, request
 import selenium_script as ss
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/smart_size', methods=['POST'])
 def smart_size():
-    # fields = [k for k in request.form]                                      
+    # fields = [k for k in request.form]
     # values = [request.form[k] for k in request.form]
     # data = dict(zip(fields, values))
-    
+
     # print(data)
 
     choice_1 = request.form['options-step-1']
@@ -26,9 +28,37 @@ def smart_size():
     choice_10 = request.form['options-step-10']
     choice_11 = request.form['options-step-11']
 
-    choices = [choice_1, choice_2, choice_3, choice_4, choice_5, 
-    choice_6, choice_7, choice_8, choice_9, choice_10, choice_11]
-    
+    # Step 2 - Something else
+    se_step_2_ft = request.form['se-step-2-ft']
+    se_step_2_in = request.form['se-step-2-in']
+    if(se_step_2_ft != "" and se_step_2_in != ""):
+        choice_2 = (se_step_2_ft, se_step_2_in)
+
+    # Step 3 - Something else
+    se_step_3_lbs = request.form['se-step-3-lbs']
+    if(se_step_3_lbs != ""):
+        choice_3 = (se_step_3_lbs, None)
+
+    # Step 5 - Something else
+    se_step_5_in = request.form['se-step-5-in']
+    if(se_step_5_in != ""):
+        choice_5 = (se_step_5_in, None)
+
+    # Step 8 - Something else
+    se_step_8_in = request.form['se-step-8-in']
+    if(se_step_8_in != ""):
+            choice_8 = (se_step_8_in, None)
+
+    # Step 9 - Something else
+    se_step_9_in = request.form['se-step-9-in']
+    if(se_step_9_in != ""):
+            choice_9 = (se_step_9_in, None)
+
+    choices = [choice_1, choice_2, choice_3, choice_4, choice_5, choice_6,
+               choice_7, choice_8, choice_9, choice_10, choice_11]
+
+    # print(choices)
+
     # print(choice_1)
     # print(choice_2)
     # print(choice_3)
@@ -41,12 +71,14 @@ def smart_size():
     # print(choice_10)
     # print(choice_11)
 
-    browser = ss.webdriver.Chrome('./chromedriver',chrome_options=ss.chrome_options)
+    browser = ss.webdriver.Chrome(  './chromedriver', chrome_options=ss.chrome_options)
     browser.get(ss.BASE_URL)
     answers = ss.get_answer(choices, browser)
     # answers = {'long_sleeve_tucked': {'Collar Around': '15.5', 'Sleeve Length': '31.75', 'Yoke Width': '17.5', 'Chest Width': '20.25', 'Midsection Width': '17.75', 'Shirt Length': '29.75', 'Sleeve Width': '8.25', 'Cuff Around': '8.75'}, 'long_sleeve_untucked': {'Collar Around': '15.5', 'Sleeve Length': '31.75', 'Yoke Width': '17.5', 'Chest Width': '20.25', 'Midsection Width': '17.75', 'Shirt Length': '28.25', 'Sleeve Width': '8.25', 'Cuff Around': '8.75'}, 'short_sleeve_untucked': {'Collar Around': '15.5', 'Short Sleeve Length': '8.5', 'Yoke Width': '17.5', 'Chest Width': '20.25', 'Midsection Width': '17.75', 'Shirt Length': '28', 'Short Sleeve Width': '8', 'Short Sleeve Opening': '7.25'}}
+    # print(answers)
 
     return render_template('smart-size.html', answers=answers)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- selenium_script.py
+++ selenium_script.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import json
 import random
 import time
@@ -31,16 +32,29 @@ def get_answer(input_params, browser):
     browser.find_element_by_xpath(steps['step_1'][input_params[0]]).click()
 
     browser.find_element_by_xpath('//*[@id="smart_size_step_1"]/nav/a').click()
-    browser.find_element_by_xpath(steps['step_2'][input_params[1]]).click()
+    if(not isinstance(input_params[1], tuple)):
+        browser.find_element_by_xpath(steps['step_2'][input_params[1]]).click()
+    else:
+        browser.find_element_by_xpath(steps['step_2']['ft']).click()
+        browser.find_element_by_xpath(steps['step_2']['ft']).send_keys(input_params[1][0])
+        browser.find_element_by_xpath(steps['step_2']['in']).send_keys(input_params[1][1])
 
     browser.find_element_by_xpath('//*[@id="smart_size_step_3"]/nav/a[2]').click()
-    browser.find_element_by_xpath(steps['step_3'][input_params[2]]).click()
+    if(not isinstance(input_params[2], tuple)):
+        browser.find_element_by_xpath(steps['step_3'][input_params[2]]).click()
+    else:
+        browser.find_element_by_xpath(steps['step_3']['lbs']).click()
+        browser.find_element_by_xpath(steps['step_3']['lbs']).send_keys(input_params[2][0])
 
     browser.find_element_by_xpath('//*[@id="smart_size_step_4"]/nav/a[2]').click()
     browser.find_element_by_xpath(steps['step_4'][input_params[3]]).click()
 
     browser.find_element_by_xpath('//*[@id="smart_size_step_5"]/nav/a[2]').click()
-    browser.find_element_by_xpath(steps['step_5'][input_params[4]]).click()
+    if(not isinstance(input_params[4], tuple)):
+        browser.find_element_by_xpath(steps['step_5'][input_params[4]]).click()
+    else:
+        browser.find_element_by_xpath(steps['step_5']['in']).click()
+        browser.find_element_by_xpath(steps['step_5']['in']).send_keys(input_params[4][0])
 
     browser.find_element_by_xpath('//*[@id="smart_size_step_7"]/nav/a[2]').click()
     browser.find_element_by_xpath(steps['step_6'][input_params[5]]).click()
@@ -49,10 +63,18 @@ def get_answer(input_params, browser):
     browser.find_element_by_xpath(steps['step_7'][input_params[6]]).click()
 
     browser.find_element_by_xpath('//*[@id="smart_size_step_14"]/nav/a[2]').click()
-    browser.find_element_by_xpath(steps['step_8'][input_params[7]]).click()
+    if(not isinstance(input_params[7], tuple)):
+        browser.find_element_by_xpath(steps['step_8'][input_params[7]]).click()
+    else:
+        browser.find_element_by_xpath(steps['step_8']['in']).click()
+        browser.find_element_by_xpath(steps['step_8']['in']).send_keys(input_params[7][0])
 
     browser.find_element_by_xpath('//*[@id="smart_size_step_9"]/nav/a[2]').click()
-    browser.find_element_by_xpath(steps['step_9'][input_params[8]]).click()
+    if(not isinstance(input_params[8], tuple)):
+        browser.find_element_by_xpath(steps['step_9'][input_params[8]]).click()
+    else:
+        browser.find_element_by_xpath(steps['step_9']['in']).click()
+        browser.find_element_by_xpath(steps['step_9']['in']).send_keys(input_params[8][0])
 
     browser.find_element_by_xpath('//*[@id="smart_size_step_10"]/nav/a[2]').click()
     browser.find_element_by_xpath(steps['step_10'][input_params[9]]).click()
@@ -67,26 +89,36 @@ def get_answer(input_params, browser):
     browser.find_element_by_xpath('//*[@id="password"]').send_keys('abc')
 
     browser.find_element_by_xpath('//*[@id="buttonCreateAccountConfirmSS"]').click()
-    
+    time.sleep(0.5)
     
     # Results
+
+    #More details button
+    more_details = browser.find_element_by_xpath('//*[@id="dimensions-cs"]/ul/span')
+    browser.execute_script("arguments[0].click();", more_details)
+
     # long_sleeve_tucked
-    # browser.find_elements_by_class_name('size-tabs')[0].find_elements_by_tag_name('a')[0].click()
-    long_sleeve_tucked_div = browser.find_element_by_xpath('//*[@id="dimensions-cs"]/ul').text.split('\n')[:-2]
+    long_sleeve_tucked_div = browser.find_element_by_xpath('//*[@id="dimensions-cs"]/ul').text.split('\n')[:-1]
+    long_sleeve_tucked_div.remove('*All dimensions shown in inches')
+    long_sleeve_tucked_div.remove('Show Less')
     keys = long_sleeve_tucked_div[::2]
     values = reversed(long_sleeve_tucked_div[::-2])
     long_sleeve_tucked = dict(zip(keys, values))
 
     # long_sleeve_untucked
     browser.find_elements_by_class_name('size-tabs')[0].find_elements_by_tag_name('a')[1].click()
-    long_sleeve_untucked_div = browser.find_element_by_xpath('//*[@id="dimensions-cs"]/ul').text.split('\n')[:-2]
+    long_sleeve_untucked_div = browser.find_element_by_xpath('//*[@id="dimensions-cs"]/ul').text.split('\n')[:-1]
+    long_sleeve_untucked_div.remove('*All dimensions shown in inches')
+    long_sleeve_untucked_div.remove('Show Less')
     keys = long_sleeve_untucked_div[::2]
     values = reversed(long_sleeve_untucked_div[::-2])
     long_sleeve_untucked = dict(zip(keys, values))
 
     # short_sleeve_untucked
     browser.find_elements_by_class_name('size-tabs')[0].find_elements_by_tag_name('a')[2].click()
-    short_sleeve_untucked_div = browser.find_element_by_xpath('//*[@id="dimensions-cs"]/ul').text.split('\n')[:-2]
+    short_sleeve_untucked_div = browser.find_element_by_xpath('//*[@id="dimensions-cs"]/ul').text.split('\n')[:-1]
+    short_sleeve_untucked_div.remove('*All dimensions shown in inches')
+    short_sleeve_untucked_div.remove('Show Less')
     keys = short_sleeve_untucked_div[::2]
     values = reversed(short_sleeve_untucked_div[::-2])
     short_sleeve_untucked = dict(zip(keys, values))
